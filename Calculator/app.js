3429a8b8a23bbd395f203d30b5c617c8487d240e
biswajitnk <91400712+biswajitnk@users.noreply.github.com>
2021-10-07 12:19:58 +0530

    calculator only javascript

---
+++ Calculator/app.js
@@ -0,0 +1,28 @@
+let screen = document.getElementById("screen");
+let buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons){
+    item.addEventListener('click', (item)=>{
+        ButtonText = item.target.innerText;
+        // console.log(ButtonText)
+        if(ButtonText == 'x'){
+            ButtonText = '*'
+            screenValue += ButtonText;
+            screen.value = screenValue;
+        }
+        else if(ButtonText == '='){
+            screen.value = eval(screenValue);
+            
+
+        }
+        else if(ButtonText == 'CE'){
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else{
+            screenValue += ButtonText;
+            screen.value = screenValue;
+        }
+    })
+}
+
